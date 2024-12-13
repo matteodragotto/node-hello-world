29af6e9f48774baff865949707aa918fb2c4d61b
matteodragotto <matteodrag8@gmail.com>
2024-12-13 15:24:07 +0100

    [feat] words from terminal

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+
+
+console.log(process.argv);
+
+const argomento = process.argv[2]
+const saluto = 'Hello'
+const salutoFull = `${saluto} ${argomento}`;
+
+console.log(salutoFull);
